7aa15166128fb74a140ab6c759424791de4b213a
rafa1771 <raf1771@gmail.com>
2023-07-23 21:36:06 +0200

    cleaned app code

--- app.py
+++ app.py
@@ -13,17 +13,16 @@ from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
 from typing import Type
 from bs4 import BeautifulSoup
-import openai
+
 import requests
 import json
-# import streamlit as st
+
 from langchain.schema import SystemMessage
 from fastapi import FastAPI
 
 load_dotenv()
 browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #1. Tool for search
 def search(query):
@@ -97,18 +96,18 @@ def summary(objective, content):
     "{text}"
     SUMMARY:
     """
-    
+
     map_prompt_template = PromptTemplate(
         template=map_prompt, input_variables=["text", "objective"])
 
-                                              
+
     summary_chain = load_summarize_chain(
         llm=llm,
         chain_type='map_reduce',
         map_prompt=map_prompt_template,
         combine_prompt=map_prompt_template,
         verbose=True
-    )   
+    )
 
     output = summary_chain.run(input_documents=docs, objective=objective)
 
@@ -116,47 +115,51 @@ def summary(objective, content):
 
 #2c. Tool to be implemente in langchain agent
 
-class ScrapeWebsiteInput(BaseModel): 
+
+class ScrapeWebsiteInput(BaseModel):
     """Inputs for scrape_website"""
-    objective: str = Field(description="The objective & task that users give to the agent") 
+    objective: str = Field(
+        description="The objective & task that users give to the agent")
     url: str = Field(description="The url of the website to be scraped")
+
 
 class ScrapeWebsiteTool(BaseTool):
     name = "scrape_website"
-    description = "useful when you need to get data from a website url, passing both url and objective"
+    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
     args_schema: Type[BaseModel] = ScrapeWebsiteInput
 
     def _run(self, objective: str, url: str):
         return scrape_website(objective, url)
-    
+
     def _arun(self, url: str):
         raise NotImplementedError("error here")
+
 
 
 
 #3. Create lanchain agent with the tools above
 
 tools = [
-        Tool(
-            name = "Search",
-            func = search,
-            description = "useful when you need to answer questions about current events, data. You should ask targeted questions"
-        ),
-        ScrapeWebsiteTool(),
+    Tool(
+        name="Search",
+        func=search,
+        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+    ),
+    ScrapeWebsiteTool(),
 ]
 
+
 system_message = SystemMessage(
-    content="""You are a world class researcher on the art market, who can do detailed research on any topic and produce facts based results; You do not make things up, you will try as hard as possible to gather facts and data to back up the research
-    
-    Please make sure you complete the objective above with the following rules:
-    1/ You should do enough research to gather as much information as possible about the objective
-    2/ If there are url of relevant links and articles, you will scrape it to gather more information
-    3/ After scraping & search, you should think "are there any new things I should search & scraping based on the data I collected ro increase research quality?" If answer is yes, continue; But don't do this process for more than 3 iterations
-    4/ You should not make things up, you should write only facts & data that you have gathered
-    5/ Your final output should use vocabulary that art dealers and art experts would understand
-    6/ In the final output, you should include all reference data & links to back up your research; You should include all reference data
-    7/ In the final output, you should include all reference data & links to back up your research; You should include all reference data
-    """
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            
+            Please make sure you complete the objective above with the following rules:
+            1/ You should do enough research to gather as much information as possible about the objective
+            2/ If there are url of relevant links & articles, you will scrape it to gather more information
+            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+            4/ You should not make things up, you should only write facts & data that you have gathered
+            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
 )
 
 agent_kwargs = {
@@ -164,20 +167,17 @@ agent_kwargs = {
     "system_message": system_message,
 }
 
-# api_key = os.getenv("OPENAI_API_KEY")
-
-
-llm = ChatOpenAI(temperature = 0, model = "gpt-3.5-turbo-16k-0613")
-
-memory = ConversationSummaryBufferMemory(memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+memory = ConversationSummaryBufferMemory(
+    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
 
 agent = initialize_agent(
     tools,
     llm,
     agent=AgentType.OPENAI_FUNCTIONS,
     verbose=True,
-    agent_kwargs = agent_kwargs,
-    memory = memory,
+    agent_kwargs=agent_kwargs,
+    memory=memory,
 )
 
 
@@ -204,7 +204,7 @@ agent = initialize_agent(
 app = FastAPI()
 
 class Query(BaseModel):
-    query: str 
+    query: str
 
 @app.post("/")
 def researchAgent(query: Query):
