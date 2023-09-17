b47f7c4699060d52032076e2efc2f1208252a3aa
rafa1771 <raf1771@gmail.com>
2023-09-17 16:07:38 +0200

    Updated prompting structure

--- app.py
+++ app.py
@@ -150,17 +150,21 @@ tools = [
 
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
-            
-            Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+    content="""
+    You are a world-class researcher skilled in gathering detailed and factual information on any topic.
+    
+    For the sender name, email address and summary provided:
+    1. Research and provide detailed information about the sender and their associated company.
+    2. Delve into the history, reputation, and main activities of the email sender.
+    3. Ensure comprehensive research to extract as much relevant information as possible.
+    4. If there are URLs or relevant links in the email, scrape them to gather more in-depth information.
+    5. If no English results are available about the sender, search in French, Spanish, Italian, and Russian. Translate any findings into English and provide a summarized version.
+    6. After each round of scraping and search, evaluate if there's any additional information that might enhance the research quality. Limit this iterative process to a maximum of 3 times.
+    7. Only present factual information without making any assumptions or fabrications.
+    8. In the final output, ensure that all claims or data points are backed by references or links.
+    """
 )
+
 
 agent_kwargs = {
     "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
